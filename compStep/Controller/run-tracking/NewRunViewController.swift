a0d0b330117a2cf75499ca3fc2458ba85b8a50dc
boredarthur <boredarthur@gmail.com>
2021-04-25 00:53:26 +0300

    ready for saving run

--- compStep/Controller/run-tracking/NewRunViewController.swift
+++ compStep/Controller/run-tracking/NewRunViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class NewRunViewController: UIViewController {
     @IBOutlet weak var DataStack: UIStackView!
@@ -17,8 +18,20 @@ class NewRunViewController: UIViewController {
     @IBOutlet weak var StartButton: UIButton!
     @IBOutlet weak var StopButton: UIButton!
     
+    private let locationManager = LocationManager.shared
+    private var seconds = 0
+    private var timer: Timer?
+    private var distance = Measurement(value: 0, unit: UnitLength.meters)
+    private var locationList: [CLLocation] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer?.invalidate()
+        locationManager.stopUpdatingLocation()
     }
     
     
@@ -47,12 +60,49 @@ class NewRunViewController: UIViewController {
         DataStack.isHidden = false
         StartButton.isHidden = true
         StopButton.isHidden = false
+        
+        seconds = 0
+        distance = Measurement(value: 0, unit: UnitLength.meters)
+        locationList.removeAll()
+        updateDisplay()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {_ in
+            self.eachSecond()
+        }
+        startLocationUpdates()
     }
     
     private func stopRun(){
         DataStack.isHidden = true
         StartButton.isHidden = false
         StopButton.isHidden = true
+        locationManager.stopUpdatingLocation()
+    }
+    
+    private func saveRun(){
+    }
+
+    func eachSecond(){
+        seconds += 1
+        updateDisplay()
+    }
+    
+    private func updateDisplay(){
+        let formattedDistance = FormatDisplay.distance(distance)
+        let formattedTime = FormatDisplay.time(seconds)
+        let formattedPace = FormatDisplay.pace(distance: distance,
+                                               seconds: seconds,
+                                               outputUnit: UnitSpeed.minutesPerKilometer)
+        
+        DistanceLabel.text = "Distance: \(formattedDistance)"
+        TimeLabel.text = "Time: \(formattedTime)"
+        PaceLabel.text = "Pace: \(formattedPace)"
+    }
+    
+    private func startLocationUpdates(){
+        locationManager.delegate = self
+        locationManager.activityType = .fitness
+        locationManager.distanceFilter = 10
+        locationManager.startUpdatingLocation()
     }
 }
 
@@ -66,6 +116,22 @@ extension NewRunViewController: SegueHandlerType{
         switch segueIdentifier(for: segue){
             case .details:
                 let destination = segue.destination as! RunDetailsViewController
+        }
+    }
+}
+
+extension NewRunViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        for newLocation in locations{
+            let howRecent = newLocation.timestamp.timeIntervalSinceNow
+            guard newLocation.horizontalAccuracy < 20 && abs(howRecent) < 10 else { continue }
+            
+            if let lastLocation = locationList.last{
+                let delta = newLocation.distance(from: lastLocation)
+                distance = distance + Measurement(value: delta, unit: UnitLength.meters)
+            }
+            
+            locationList.append(newLocation)
         }
     }
 }
