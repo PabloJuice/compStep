550c073247a6c097a59f754523cb80ffb948b5db
boredarthur <boredarthur@gmail.com>
2021-04-24 15:44:30 +0300

    init commit

---
+++ compStep/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  compStep
+//
+//  Created by boredarthur on 24.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func hello(_ sender: UIButton) {
+        print("hello")
+    }
+    
+
+}
+
